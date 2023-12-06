d6d7e68f20428edc990cd64aad92040dbb30e3aa
Basith-m <abm2k20@gmail.com>
2023-12-06 12:56:29 +0530

    form validation completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { AddUserComponent } from './modules/users/add-user/add-user.component';
 
 const routes: Routes = [
   // http://localhost:4200/users
